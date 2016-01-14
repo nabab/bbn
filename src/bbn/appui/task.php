885eaea1a3509bb1f91094910d48fed2b363493f
nabab <thomas.nabet@gmail.com>
2016-01-14 10:10:28 +0100

    Changes bugs to tasks

--- src/bbn/appui/task.php
+++ src/bbn/appui/task.php
@@ -13,8 +13,8 @@ class task {
 
   private $db;
 
-  private function _email($id_bug, $subject, $text){
-    $users = array_unique(array_merge($this->get_ccs($id_bug), $this->mgr->get_users(1)));
+  private function _email($id_task, $subject, $text){
+    $users = array_unique(array_merge($this->get_ccs($id_task), $this->mgr->get_users(1)));
     foreach ( $users as $u ){
       if ( ($u !== $this->id_user) && ($email = $this->mgr->get_email($u)) ){
         $this->db->insert('apst_accuses', [
@@ -36,25 +36,25 @@ class task {
 
   public function info($id){
     return $this->db->get_row("
-      SELECT bbn_bugs.*, COUNT(DISTINCT bbn_bugs_comments.id) AS `comments`, `status`, crea,
-      GREATEST(crea, MAX(bbn_bugs_comments.creation_date)) AS `last_activity`,
+      SELECT bbn_tasks.*, COUNT(DISTINCT bbn_tasks_comments.id) AS `comments`, `status`, crea,
+      GREATEST(crea, MAX(bbn_tasks_comments.creation_date)) AS `last_activity`,
       TIMESTAMPDIFF(
         SECOND,
-        bbn_bugs.creation_date,
-        IF ( status != 'résolu', NOW(), GREATEST(crea, MAX(bbn_bugs_comments.creation_date)) )
+        bbn_tasks.creation_date,
+        IF ( status != 'résolu', NOW(), GREATEST(crea, MAX(bbn_tasks_comments.creation_date)) )
       ) AS `duration`
-      FROM `bbn_bugs`
+      FROM `bbn_tasks`
         JOIN (
-              SELECT id_bug, `status`, creation_date AS crea
-              FROM bbn_bugs_status
-              WHERE id_bug = ?
+              SELECT id_task, `status`, creation_date AS crea
+              FROM bbn_tasks_status
+              WHERE id_task = ?
               ORDER BY creation_date DESC
           ) AS statuses
-            ON statuses.id_bug = bbn_bugs.id
-        JOIN bbn_bugs_comments
-          ON bbn_bugs_comments.id_bug = bbn_bugs.id
-      WHERE bbn_bugs.id = ?
-      GROUP BY bbn_bugs.id",
+            ON statuses.id_task = bbn_tasks.id
+        JOIN bbn_tasks_comments
+          ON bbn_tasks_comments.id_task = bbn_tasks.id
+      WHERE bbn_tasks.id = ?
+      GROUP BY bbn_tasks.id",
       $id);
   }
 
@@ -63,12 +63,12 @@ class task {
   }
 
   public function get_ccs($id){
-    return $this->db->get_column_values('bbn_bugs_cc', 'id_user', ['id_bug' => $id]);
+    return $this->db->get_column_values('bbn_tasks_cc', 'id_user', ['id_task' => $id]);
   }
 
   public function comment($id, $text){
-    if ( ($info = $this->info($id)) && $this->db->insert('bbn_bugs_comments', [
-      'id_bug' => $id,
+    if ( ($info = $this->info($id)) && $this->db->insert('bbn_tasks_comments', [
+      'id_task' => $id,
       'creation_date' => date('Y-m-d H:i:s'),
       'id_user' => $this->id_user,
       'comment' => $text
@@ -87,26 +87,26 @@ class task {
 
   public function insert($title, $text, $target = false){
     $date = date('Y-m-d H:i:s');
-    if ( $this->db->insert('bbn_bugs', [
+    if ( $this->db->insert('bbn_tasks', [
       'title' => $title,
       'id_user' => $this->id_user,
       'target_date' => empty($target) ? null : $target,
       'creation_date' => $date
     ]) ){
       $id = $this->db->last_id();
-      $this->db->insert('bbn_bugs_status', [
-        'id_bug' => $id,
+      $this->db->insert('bbn_tasks_status', [
+        'id_task' => $id,
         'creation_date' => $date,
         'id_user' => $this->id_user,
         'status' => 'ouvert'
       ]);
-      $this->db->insert('bbn_bugs_comments', [
-        'id_bug' => $id,
+      $this->db->insert('bbn_tasks_comments', [
+        'id_task' => $id,
         'creation_date' => $date,
         'id_user' => $this->id_user,
         'comment' => $text
       ]);
-      $this->db->insert('bbn_bugs_cc', ['id_user' => $this->id_user, 'id_bug' => $id]);
+      $this->db->insert('bbn_tasks_cc', ['id_user' => $this->id_user, 'id_task' => $id]);
       $subject = "Nouveau bug posté par {$this->user}";
       $text = "<p>{$this->user} a posté un nouveau bug</p>".
         "<p><strong>$title</strong></p>".
@@ -122,7 +122,7 @@ class task {
       $date = date('Y-m-d H:i:s');
       $pretext = "<p>{$this->user} a procédé aux changements suivants concernant le bug<br> <strong>$title</strong>:</p>";
       $text = '';
-      if ( $this->db->update("bbn_bugs", [
+      if ( $this->db->update("bbn_tasks", [
         'title' => $title,
         'target_date' => empty($target) ? null : $target,
         'priority' => $priority
@@ -144,8 +144,8 @@ class task {
         }
       }
       if ( $status !== $info['status'] ){
-        $this->db->insert('bbn_bugs_status', [
-          'id_bug' => $id,
+        $this->db->insert('bbn_tasks_status', [
+          'id_task' => $id,
           'creation_date' => $date,
           'id_user' => $this->id_user,
           'status' => $status
@@ -162,7 +162,7 @@ class task {
   }
 
   public function delete($id){
-    if ( ($info = $this->info($id)) && $this->db->delete('bbn_bugs', ['id' => $id]) ){
+    if ( ($info = $this->info($id)) && $this->db->delete('bbn_tasks', ['id' => $id]) ){
       $subject = "Suppression du bug $info[title]";
       $text = "<p>{$this->user} a supprimé le bug<br><strong>$info[title]</strong></p>";
       $this->_email($id, $subject, $text);
@@ -183,10 +183,10 @@ class task {
   }
 
   public function subscribe($id){
-    return $this->db->insert('bbn_bugs_cc', ['id_user' => $this->id_user, 'id_bug' => $id]);
+    return $this->db->insert('bbn_tasks_cc', ['id_user' => $this->id_user, 'id_task' => $id]);
   }
 
   public function unsubscribe($id){
-    return $this->db->delete('bbn_bugs_cc', ['id_user' => $this->id_user, 'id_bug' => $id]);
+    return $this->db->delete('bbn_tasks_cc', ['id_user' => $this->id_user, 'id_task' => $id]);
   }
 }
