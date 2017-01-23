e06b9ece27311d42e9d1ebe23451b648c2a6cfc6
nabab <thomas.nabet@gmail.com>
2017-01-23 14:48:47 +0100

    Optional error doing end(explode())

--- src/bbn/models/tts/optional.php
+++ src/bbn/models/tts/optional.php
@@ -36,7 +36,8 @@ trait optional
           die("Impossible to find the option appui for ".__CLASS__);
         }
       }
-      $cls = end(explode("\\", __CLASS__));
+      $tmp = explode("\\", __CLASS__);
+      $cls = end($tmp);
       self::$option_root_id = $opt->from_code($cls, BBN_APPUI);
       if ( !self::$option_root_id ){
         die("Impossible to find the option $cls for ".__CLASS__);
