f4f4d4ab52b39a0c3d76618dad6f518905aafda4
Amr Shawky <amrshawky@gmail.com>
2021-05-20 03:12:27 +0200

    MVC test

--- tests/Mockable.php
+++ tests/Mockable.php
@@ -6,19 +6,20 @@ trait Mockable
 {
 
 
-    /**
-     * @param string $class
-     * @param string $method
-     * @param        $value
-     *
-     * @return \Mockery\MockInterface
-     */
-  protected function mockClassMethod(string $class, string $method, $value)
+  /**
+   * @param string $class
+   * @param string $method
+   * @param $value
+   * @param string $times
+   * @return \Mockery\MockInterface
+   */
+  protected function mockClassMethod(string $class, string $method, $value, string $times = 'once')
   {
       return ReflectionHelpers::mockClassMethod(
         $class,
         $method,
-        $value
+        $value,
+        $times
       );
   }
 
