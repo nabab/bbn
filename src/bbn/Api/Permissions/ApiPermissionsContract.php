4fe6c08852c00a2b7f54e8a5de7ffdfe70197bab
Amr Shawky <amrshawky@gmail.com>
2021-06-22 19:52:08 +0200

    Api auth and verification

--- src/bbn/Api/Permissions/ApiPermissionsContract.php
+++ src/bbn/Api/Permissions/ApiPermissionsContract.php
@@ -9,4 +9,6 @@ interface ApiPermissionsContract
   public function getTokens(string $authorization_code): array;
 
   public function refreshAccessToken(string $refresh_token);
+
+  public function getProvider();
 }
