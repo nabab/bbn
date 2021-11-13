da4765ac5b972ee283ab51b082e420d64881eca3
Heni <heniabida220@gmail.com>
2021-11-13 10:06:37 +0000

    Update exec_unittests.sh

--- exec_unittests.sh
+++ exec_unittests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "------------------------------------- Starting PHPUnit tests -----------------------------------------------"
-./vendor/bin/phpunit tests/* --log-junit=junit_report.xml --coverage-xml=coverage_php.xml
+./vendor/bin/phpunit tests/* --log-junit=junit_report.xml
+./vendor/bin/phpunit tests/* --coverage-xml=coverage_php.xml
 echo "------------------------------------- Endof PHPUnit tests -----------------------------------------------"
