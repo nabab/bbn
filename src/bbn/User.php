8632ba783dcedcab9b07d1c520f126b84fd7ed0d
Mirko Argentino <mirko@bbn.solutions>
2021-11-25 16:35:34 +0100

    Fixed device_lang on User class

--- src/bbn/User.php
+++ src/bbn/User.php
@@ -386,11 +386,14 @@ class User extends Models\Cls\Basic
           throw new \Exception(X::_('Invalid token').' '.$params[$f['token']].' / '.$params[$f['device_uid']]);
         }
 
-        // Update device_lang and last 
-        $this->db->update($this->class_cfg['tables']['api_tokens'], [
-          $this->class_cfg['arch']['api_tokens']['device_lang'] => $params[$f['device_lang']] ?? '',
+        // Update device_lang and last
+        $toUdp = [
           $this->class_cfg['arch']['api_tokens']['last'] => date('Y-m-d H:i:S')
-        ], [
+        ];
+        if (isset($params[$f['device_lang']])) {
+          $toUdp[$this->class_cfg['arch']['api_tokens']['device_lang']] = $params[$f['device_lang']];
+        }
+        $this->db->update($this->class_cfg['tables']['api_tokens'], $toUdp, [
           $this->class_cfg['arch']['api_tokens']['token']      => $params[$f['token']],
           $this->class_cfg['arch']['api_tokens']['device_uid'] => $params[$f['device_uid']]
         ]);
