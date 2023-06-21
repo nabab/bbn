64e140758f636e63128e7e6b1c7c1bb7dd701e97
Loredana Bruno <loredana@bbn.so>
2023-06-21 12:26:02 +0200

    Add remove providers emails

--- src/bbn/Shop/Provider.php
+++ src/bbn/Shop/Provider.php
@@ -53,7 +53,8 @@ class Provider extends DbCls
     ],
     'table' => 'bbn_shop_providers',
     'tables' => [
-      'providers' => 'bbn_shop_providers'
+      'providers' => 'bbn_shop_providers',
+      'emails' => 'bbn_shop_providers_emails'
     ],
     'arch' => [
       'providers' => [
@@ -61,10 +62,57 @@ class Provider extends DbCls
         'name' => 'name',
         'cfg' => 'cfg'
       ],
-    ],
+      'emails' => [
+        'id_provider' => 'id_provider',
+        'email' => 'email'
+      ]
+    ]
   ];
 
+  protected function providerGetEmails(string $id_provider) :?array
+  { 
+    $cfg = $this->getClassCfg();
+    return $this->db->rselectAll($cfg['tables']['emails'], 'email', [
+      $cfg['arch']['emails']['id_provider'] => $id_provider
+    ]);
+  }
 
+  protected function providerEmailExists(string $id_provider, string $email) :?bool
+  { 
+    $cfg = $this->getClassCfg();
+    if ( $this->db->rselect($cfg['tables']['emails'], 'email', [
+      $cfg['arch']['emails']['id_provider'] => $id_provider,
+      $cfg['arch']['emails']['email'] => $email
+    ])) {
+      return true;
+    }
+    return false;
+  }
+
+  public function addProviderEmail(string $id_provider, string $email) 
+  { 
+    $cfg = $this->getClassCfg();
+    $chars = ' \n\r\t\v\x00';
+    if (!$this::providerEmailExists($id_provider, $email)) {
+      return $this->db->insert($cfg['tables']['emails'], [
+        $cfg['arch']['emails']['email'] => trim($email, $chars),
+        $cfg['arch']['emails']['id_provider'] => $id_provider
+      ]);
+    }
+  }
+
+  public function deleteProviderEmail(string $id_provider, string $email) 
+  { 
+    $cfg = $this->getClassCfg();
+    if ($this::providerEmailExists($id_provider, $email)) {
+      
+      return $this->db->delete($cfg['tables']['emails'], [
+        $cfg['arch']['emails']['email'] => $email,
+        $cfg['arch']['emails']['id_provider'] => $id_provider
+      ]);
+    }
+  }
+  
   public function __construct(Db $db, array $cfg = null)
   {
     // The database connection
@@ -96,7 +144,10 @@ class Provider extends DbCls
   public function edit($id, array $data): ?string
   {
     $dbcfg = $this->getClassCfg();
-    if (X::hasProp($data, 'name', true)) {
+    if (X::hasProp($data, 'name', true)) { 
+      if (X::hasProp($data, 'email', true) && !$this::providerEmailExists($id, $data['email'])) {
+        $this->addProviderEmail($id, $data['email']);
+      }
       return $this->db->update(
         $dbcfg['table'],
         [
@@ -114,6 +165,10 @@ class Provider extends DbCls
   public function get(string $id): ?array
   {
     if ($res = $this->rselect($id)) {
+      $dbcfg = $this->getClassCfg();
+      $res['emails'] = $this->db->rselectAll($dbcfg['tables']['emails'], 'email', [
+        $dbcfg['arch']['emails']['id_provider'] => $id
+      ]);
       $res['cfg'] = $res['cfg'] ? json_decode($res['cfg'], true) : [];
       return $res;
     }
