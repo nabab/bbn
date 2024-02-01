09c37b7747ea7907c70666c2adf23e674297af2b
Mirko Argentino <mirko@bbn.so>
2024-02-01 11:58:15 +0100

    Added session.cookie_lifetime

--- src/bbn/User/Session.php
+++ src/bbn/User/Session.php
@@ -291,6 +291,7 @@ class Session
 
         if (defined('BBN_SESS_LIFETIME')) {
           ini_set('session.gc_maxlifetime', BBN_SESS_LIFETIME);
+          ini_set('session.cookie_lifetime', BBN_SESS_LIFETIME);
         }
       }
 
