ac0c9b35d76afa58b1c5214b710e2487359366b2
nabab <thomas.nabet@gmail.com>
2025-01-28 00:21:31 +0100

    Fix

--- src/bbn/Appui.php
+++ src/bbn/Appui.php
@@ -2478,7 +2478,7 @@ class Appui
       if (!empty($settings['history'])) {
         $installer->report(X::_("History update starting, it might take a while..."));
         foreach ($this->updateHistory() as $res) {
-          $installer->report("{$res} entries imported");
+          $installer->report("%s entries imported", $res);
         }
 
         $installer->report(X::_("History update successful"));
