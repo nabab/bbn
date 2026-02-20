a1eeee0c7fb64007533056009b1e8da83f2450a1
nabab <thomas.nabet@gmail.com>
2026-02-20 10:43:51 +0100

    Only DbActions in EntityTable

--- src/bbn/Entities/Models/EntityTable.php
+++ src/bbn/Entities/Models/EntityTable.php
@@ -3,11 +3,11 @@ namespace bbn\Entities\Models;
 
 
 use bbn\Models\Cls\Db as DbCls;
-use bbn\Models\Tts\DbActionsPublic;
+use bbn\Models\Tts\DbActions;
 use bbn\Entities\Models\EntityTrait;
 
 abstract class EntityTable extends DbCls
 {
-  use DbActionsPublic;
+  use DbActions;
   use EntityTrait;
 }
